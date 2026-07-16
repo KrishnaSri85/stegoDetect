55fe772f7823966462dad290bac5dbd8b84651e5
Krishna sri Sikhakolli <skrishnasri123@gmail.com>
2026-07-16 10:55:20 +0530

    initial test

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-import qiskit
-print(qiskit.__version__)
+from qiskit import QuantumCircuit
+import numpy as np
+normalized=[0.0213, 1.0, 0.0013, 0.0, 0.0021, 0.0021]
+qc = QuantumCircuit(len(normalized))
+
+for i, value in enumerate(normalized):
+    qc.ry(value * np.pi, i)
+
+print(qc)
